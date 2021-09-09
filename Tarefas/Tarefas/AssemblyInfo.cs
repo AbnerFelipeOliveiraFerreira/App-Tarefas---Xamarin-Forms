cac4c70157f4a3a6c3500eae4eee3056ddd8b4fd
Abner Felipe <desenvolvimento.abnfel@outlook.com>
2021-09-09 13:34:49 -0300

    Tela01 - OK

--- Tarefas/Tarefas/AssemblyInfo.cs
+++ Tarefas/Tarefas/AssemblyInfo.cs
@@ -1,3 +1,9 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Montserrat-Bold.ttf", Alias = "Montserrat-Bold")]
+[assembly: ExportFont("Montserrat-Medium.ttf", Alias = "Montserrat-Medium")]
+[assembly: ExportFont("Montserrat-Regular.ttf", Alias = "Montserrat-Regular")]
+[assembly: ExportFont("Montserrat-Thin.ttf", Alias = "Montserrat-Thin")]
+[assembly: ExportFont("Montserrat-Light.ttf", Alias = "Montserrat-Light")]
